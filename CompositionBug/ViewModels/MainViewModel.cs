aa53d77cc40a8b9dda36ca39b5751e42ba9a294c
laolarou <luolinrui726@outlook.com>
2024-03-23 16:01:29 -0700

    添加项目文件。

---
+++ CompositionBug/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace CompositionBug.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ CompositionBug/Views/MainView.axaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Numerics;
+using Avalonia.Animation.Easings;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Rendering.Composition;
+
+namespace CompositionBug.Views;
+
+public partial class MainView : UserControl
+{
+    private bool _flag;
+
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        base.OnLoaded(e);
+
+        BeginAnimation(false);
+    }
+
+    private void BeginAnimation(bool flag)
+    {
+        var easing = new SplineEasing(0.1, 0.9, 0.2);
+
+        var from = flag ? new Vector3(-500, 225, 0) : new Vector3(150, 225, 0);
+        var to = flag ? new Vector3(150, 225, 0) : new Vector3(-500, 225, 0);
+
+        var visual = ElementComposition.GetElementVisual(Rect)!;
+        var compositor = visual.Compositor;
+        var ani = compositor.CreateVector3KeyFrameAnimation();
+
+        ani.InsertKeyFrame(0f, from, easing);
+        ani.InsertKeyFrame(1f, to, easing);
+
+        ani.Duration = TimeSpan.FromMilliseconds(445);
+
+        visual.StartAnimation("Offset", ani);
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        _flag = !_flag;
+        BeginAnimation(_flag);
+    }
+}
